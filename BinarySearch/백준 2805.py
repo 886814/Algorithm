2a194aec5aebde1f0d32b881cbf88650b383d0db
Chul Ho Shin <90623647+886814@users.noreply.github.com>
2022-09-22 00:04:46 +0900

    Update 백준 2805.py

--- BinarySearch/백준 2805.py
+++ BinarySearch/백준 2805.py
@@ -1,6 +1,6 @@
 n,m = map(int,input().split())
 a = sorted(list(map(int,input().split())))
-lt,rt = a[0],a[-1]
+lt,rt = 0,a[-1]
 while lt<=rt:
     mid = (rt+lt)//2
     temp = sum([x-mid for x in a if x > mid])
