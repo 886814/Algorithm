9fa5e6b5918b2a7ebf021102f5922ee8d9a22d0f
Chul Ho Shin <90623647+886814@users.noreply.github.com>
2022-09-24 21:36:33 +0900

    Add files via upload

---
+++ 카카오 1번.py
@@ -0,0 +1,26 @@
+def time(s):
+    temp = list(map(int,s.split(".")))
+    year = (temp[0]-2000)*12*28
+    month = temp[1] * 28
+    print(year + month + temp[-1])
+    return year + month + temp[-1]
+
+ans = []
+today = "2020.01.01"
+terms = ["Z 3", "D 5"]
+terms_new = {}
+for term in terms:
+    x,y = term.split()
+    terms_new[x]=int(y)
+
+privacies = ["2019.01.01 D", "2019.11.15 Z", "2019.08.02 D","2019.07.01 D", "2018.12.28 Z"]
+today = time(today)
+
+# for privacy in privacies:
+#     time_, term = privacy.split()
+#     time_ = time(time_) + terms_new[term]*28
+    
+new_ = [time(privacy.split()[0]) + terms_new[privacy.split()[1]]*28 for privacy in privacies]
+for i in range(len(new_)):
+    if new_[i] <= today:
+        ans.append(i+1)
