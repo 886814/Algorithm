ec8aaeeb4f8bf9239030c8a73d10a8243f944668
Chul Ho Shin <90623647+886814@users.noreply.github.com>
2022-10-05 23:53:02 +0900

    Add files via upload

---
+++ BinarySearch/백준 2512.py
@@ -0,0 +1,13 @@
+n = int(input())
+budgets = list(map(int,input().split()))
+total = int(input())
+budgets.sort()
+lt,rt = 0, budgets[-1]
+while lt<=rt:
+    mid = (lt+rt)//2
+    sum_ = sum([mid if x >= mid else x for x in budgets])
+    if sum_ > total:
+        rt = mid-1
+    else:
+        lt = mid+1
+print(mid)
