f08d1246b320d4cad0fe33e68f16756579d48bc8
Chul Ho Shin <90623647+886814@users.noreply.github.com>
2022-10-06 00:00:14 +0900

    Update 백준 2512.py

--- BinarySearch/백준 2512.py
+++ BinarySearch/백준 2512.py
@@ -3,11 +3,13 @@ budgets = list(map(int,input().split()))
 total = int(input())
 budgets.sort()
 lt,rt = 0, budgets[-1]
+ans = 0
 while lt<=rt:
     mid = (lt+rt)//2
     sum_ = sum([mid if x >= mid else x for x in budgets])
-    if sum_ > total:
-        rt = mid-1
-    else:
+    if sum_ <= total:
+        ans = mid
         lt = mid+1
-print(mid)
+    else:
+        rt = mid-1
+print(ans)
