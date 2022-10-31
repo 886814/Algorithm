43308598d5d1999d704ab3a11b9586f141bfe13d
Chul Ho Shin <90623647+886814@users.noreply.github.com>
2022-11-01 00:04:19 +0900

    Update 백준 13913.py

--- DFS,BFS/백준 13913.py
+++ DFS,BFS/백준 13913.py
@@ -1,10 +1,8 @@
 from collections import deque
 n,k = map(int,input().split())
 MAX = 100000
-ch = [0] * (MAX+1)
 dis = [0] * (MAX+1)
 route = [0] * (MAX+1)
-ch[n] = 1
 dis[n] = 0
 dQ = deque()
 dQ.append(n)
@@ -18,10 +16,9 @@ while dQ:
             temp = route[temp]
         break
     for next in (now+1, now-1, 2*now):
-        if 1 <= next <= MAX:
-            if ch[next]==0:
+        if 0 <= next <= MAX:
+            if dis[next]==0:
                 dQ.append(next)
-                ch[next] = 1
                 route[next] = now
                 dis[next] = dis[now]+1
   
