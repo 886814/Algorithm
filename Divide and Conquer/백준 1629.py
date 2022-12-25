81804942d479299694873d0ea4e5f126c5d6f417
Chul Ho Shin <90623647+886814@users.noreply.github.com>
2022-12-25 23:38:42 +0900

    Update 백준 1629.py

--- Divide and Conquer/백준 1629.py
+++ Divide and Conquer/백준 1629.py
@@ -12,3 +12,8 @@ def DivideNConquer (a,n):
           return (tmp  * tmp *a) %c
           
 print(DivideNConquer(a,b))
+
+# 별해
+import sys
+a,b,c = map(int,sys.stdin.readline().split())
+print(pow(a,b,c))
