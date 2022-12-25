ed441f71e3870c67d2715cca4aba99129856b0c6
Chul Ho Shin <90623647+886814@users.noreply.github.com>
2022-12-25 23:28:03 +0900

    Add files via upload

---
+++ 백준 1629.py
@@ -0,0 +1,14 @@
+import sys
+a,b,c = map(int,sys.stdin.readline().split())
+
+def DivideNConquer (a,n):
+  if n == 1:
+      return a%c
+  else:
+      tmp = DivideNConquer(a,n//2)
+      if n %2 ==0:
+          return (tmp * tmp) % c
+      else:
+          return (tmp  * tmp *a) %c
+          
+print(DivideNConquer(a,b))
