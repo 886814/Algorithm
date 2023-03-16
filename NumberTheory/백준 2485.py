f03f2567672b36471e12e6eafbca604442ec5df9
Chul Ho Shin <90623647+886814@users.noreply.github.com>
2023-03-16 13:47:14 +0900

    Update 백준 2485.py

--- NumberTheory/백준 2485.py
+++ NumberTheory/백준 2485.py
@@ -1,9 +1,5 @@
 import math
 n = int(input())
-trees = []
-tmp = 0
-for i in range(n):
-    x = int(input())
-    trees.append(x)
+trees = [int(input()) for _ in range(n)]
 gcd = math.gcd(*[trees[j+1] - trees[j] for j in range(len(trees)-1)])
 print((trees[-1] - trees[0]) // gcd - n + 1)
