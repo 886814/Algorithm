1ac352b204362eb447d1bec176d6fa37aca14110
Chul Ho Shin <90623647+886814@users.noreply.github.com>
2023-05-10 17:41:04 +0900

    Add files via upload

---
+++ Heap/백준 11286.py
@@ -0,0 +1,14 @@
+import heapq
+import sys
+input = sys.stdin.readline
+n = int(input())
+q = []
+for i in range(n):
+    num = int(input())
+    if num == 0:
+        try:
+            print(heapq.heappop(q)[1])
+        except:
+            print(0)
+    else:
+        heapq.heappush(q,(abs(num),num))
