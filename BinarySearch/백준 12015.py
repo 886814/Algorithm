31cbebd0ba1989d498c443c7233e097ad172f40d
Chul Ho Shin <90623647+886814@users.noreply.github.com>
2023-10-04 15:41:05 +0900

    Create 백준 12015.py

---
+++ BinarySearch/백준 12015.py
@@ -0,0 +1,17 @@
+n = int(input())
+a = list(map(int,input().split()))
+lis = [a[0]]
+for num in a[1:]:
+    if num > lis[-1]:
+        lis.append(num)
+    else:
+        lt, rt = 0, len(lis)
+        while lt < rt:
+            mid = lt + (rt-lt)//2
+            if lis[mid] < num:
+                lt = mid + 1
+            else:
+                rt = mid
+        else:
+            lis[rt] = num
+print(len(lis))
