3585217a8dcc0f1f260283cf4323f5cc864f0feb
Chul Ho Shin <90623647+886814@users.noreply.github.com>
2023-10-04 16:54:37 +0900

    Update 백준 12015.py

--- BinarySearch/백준 12015.py
+++ BinarySearch/백준 12015.py
@@ -15,3 +15,15 @@ for num in a[1:]:
         else:
             lis[rt] = num
 print(len(lis))
+
+# 별해
+from bisect import bisect_left
+n = int(input())
+a = list(map(int,input().split()))
+lis = [a[0]]
+for num in a[1:]:
+    if num > lis[-1]:
+        lis.append(num)
+    else:
+        lis[bisect_left(lis, num)] = num       
+print(len(lis))
