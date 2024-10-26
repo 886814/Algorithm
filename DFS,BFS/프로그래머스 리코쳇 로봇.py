e66a07431a10c1f923508436b1542dd9213c42c1
Chul Ho Shin <90623647+886814@users.noreply.github.com>
2024-10-26 23:22:39 +0900

    Update 프로그래머스 리코쳇 로봇.py

--- DFS,BFS/프로그래머스 리코쳇 로봇.py
+++ DFS,BFS/프로그래머스 리코쳇 로봇.py
@@ -3,12 +3,14 @@ def solution(board):
     dx = [1,0,-1,0]
     dy = [0,-1,0,1]
     r,c = len(board), len(board[0])
+    visited = set()
 
     # 출발 좌표 뽑기
     for i in range(r):
         for j in range(c):
             if board[i][j] == 'R':
                 start = (i,j,0)
+                visited.add((i,j))
     q = deque([start])
 
     # 미끄러진 위치 계산
@@ -20,18 +22,15 @@ def solution(board):
 
     # BFS
     def BFS(q):
-        ans = -1
-        visited = [[False] * c for _ in range(r)]
-        flag = True
         while q:
             x, y, cnt = q.popleft()
             for k in range(4):
                 nx, ny = move(x, y, dx[k], dy[k])
                 if board[nx][ny] == 'G':
                     return cnt + 1
-                if not (x,y) == (nx,ny) and visited[nx][ny] == False:
+                if (nx,ny) not in visited:
                     q.append((nx,ny,cnt+1))
-                    visited[nx][ny] = True
-        return ans
+                    visited.add((nx,ny))
+        return -1
 
     return BFS(q)
